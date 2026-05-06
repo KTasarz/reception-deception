6b7816076b44e57188133621b4346af8c7c0919d
KTasarz <93345869+KTasarz@users.noreply.github.com>
2026-05-06 03:42:33 +0200

    Added a better quests template loader

--- classes/quests_generator.gd
+++ classes/quests_generator.gd
@@ -1,7 +1,39 @@
 extends Node
 class_name QuestsGenerator
 
-@export var all_quest_templates : Array[QuestTemplate]
+var all_quest_templates : Array[QuestTemplate] = []
+
+func _ready():
+	# Przy starcie gry wywołujemy naszą funkcję ładującą
+	load_templates_from_folder("res://assets/templates/quests/")
+
+func load_templates_from_folder(path: String):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		
+		while file_name != "":
+			# Pomijamy foldery i upewniamy się, że to plik zasobu
+			# Uwaga: używamy trim_suffix(".remap"), bo po wyeksportowaniu gry 
+			# Godot może dopisać tę końcówkę do plików .tres!
+			var clean_name = file_name.trim_suffix(".remap")
+			
+			if not dir.current_is_dir() and clean_name.ends_with(".tres"):
+				# Tworzymy pełną ścieżkę do pliku
+				var full_path = path + "/" + file_name
+				# Ładujemy plik z dysku i rzutujemy na naszą klasę
+				var loaded_template = load(full_path) as QuestTemplate
+				
+				if loaded_template:
+					all_quest_templates.append(loaded_template)
+					
+			# Przechodzimy do kolejnego pliku w folderze
+			file_name = dir.get_next()
+			
+		print("Pomyślnie wczytano ", all_quest_templates.size(), " szablonów questów.")
+	else:
+		push_error("Błąd: Nie można otworzyć folderu z questami pod ścieżką: ", path)
 
 func generate_quest(npc : QuestsGivers) -> Quest:
 	var quest = Quest.new()
