a9b5c27d9cab9a3f233018490618b66de44fd695
KTasarz <93345869+KTasarz@users.noreply.github.com>
2026-05-07 05:34:56 +0200

    hotfix
    fixing a oversight on dragging a item (a whole ass scene was moving)

--- scripts/prototype_graphics.gd
+++ scripts/prototype_graphics.gd
@@ -13,7 +13,7 @@ func _on_wine_gui_input(event: InputEvent) -> void:
 			is_dragging = true
 			# Obliczamy różnicę między pozycją myszki a lewym górnym rogiem kartki.
 			# Dzięki temu kartka nie "przeskoczy" środkiem do kursora, tylko złapiemy ją tam, gdzie kliknęliśmy.
-			drag_offset = get_global_mouse_position() - global_position
+			drag_offset = get_global_mouse_position() - $Wine.position
 			
 			# Ważne: Przenosi kartkę na sam wierzch interfejsu, żeby nie wsuwała się pod inne papiery!
 			move_to_front() 
@@ -24,4 +24,4 @@ func _on_wine_gui_input(event: InputEvent) -> void:
 func _input(event: InputEvent) -> void:
 	if is_dragging and event is InputEventMouseMotion:
 		# Aktualizujemy pozycję kartki z uwzględnieniem miejsca, za które ją złapaliśmy
-		global_position = get_global_mouse_position() - drag_offset
+		$Wine.position = get_global_mouse_position() - drag_offset
