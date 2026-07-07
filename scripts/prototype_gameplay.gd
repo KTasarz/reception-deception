aac9ea3f6207828837fd2d4bdf8a3ce0c48c716f
KTasarz <93345869+KTasarz@users.noreply.github.com>
2026-07-08 01:14:25 +0200

    Added location in writing scroll
    Location name is now randomly selected
    Location is now transfered from draft
    fixing a couple typos

--- scripts/prototype_gameplay.gd
+++ scripts/prototype_gameplay.gd
@@ -17,6 +17,7 @@ var current_draft_form : WritingQuest = null
 func _ready() -> void:
 	label_money.text = "Money: "+ str(GameStatsManager.money)
 	label_guild_level.text = "Level: "+ str(GameStatsManager.guild_level)
+	WorldManager.generate_new_world_names_locations()
 
 func _on_button_exit_pressed() -> void:
 	SceneManager.change_scene("res://scenes/prototype_menu.tscn")
--- scripts/writing_quest.gd
+++ scripts/writing_quest.gd
@@ -5,6 +5,8 @@ var is_dragging : bool = false
 var drag_offset : Vector2 = Vector2.ZERO
 @export var map : PackedScene
 
+var location : QuestsManager.location
+
 signal open_map_requested(paper_instance : WritingQuest)
 
 func _on_gui_input(event: InputEvent) -> void:
@@ -31,12 +33,35 @@ func _on_location_pressed() -> void:
 	open_map_requested.emit(self)
 
 func set_location_text(location_name : String) -> void:
-	$VBoxContainer/HBoxContainer3/Location.text = "Location: " + location_name
+	var pre_format_text : String = "Location: {location}"
+	var translated_location : String
+	var location_key : String
+	match location_name:
+		"CAPITAL":
+			location_key = WorldManager.current_locations_names[QuestsManager.location.CAPITAL_CITY]
+			location = QuestsManager.location.CAPITAL_CITY
+		"FOREST":
+			location_key = WorldManager.current_locations_names[QuestsManager.location.FOREST]
+			location = QuestsManager.location.FOREST
+		"CAVE":
+			location_key = WorldManager.current_locations_names[QuestsManager.location.CAVE]
+			location = QuestsManager.location.CAVE
+		"VALLEY":
+			location_key = WorldManager.current_locations_names[QuestsManager.location.VALLEY]
+			location = QuestsManager.location.VALLEY
+		"RIVER":
+			location_key = WorldManager.current_locations_names[QuestsManager.location.RIVER]
+			location = QuestsManager.location.RIVER
+		_:
+			print("Something went wrong with getting a name of location!")
+			return
+	translated_location = tr(location_key)
+	var format_text : String = pre_format_text.format({"location": translated_location})
+	$VBoxContainer/HBoxContainer3/Location.text = format_text
 	
-	# Główna scena wywoła to i wrzuci tu swojego Questa
 func update_quest_data(quest_to_update: Quest) -> void:
 	quest_to_update.quest_name = $VBoxContainer/QuestTitle.text
 	quest_to_update.description = $VBoxContainer/QuestDescription.text
 	quest_to_update.player_set_reward = float($VBoxContainer/Reward.value)
 	quest_to_update.player_set_required_level = int($VBoxContainer/Level.value)
-	#quest_to_update.player_set_location = 
+	quest_to_update.player_set_location = location
